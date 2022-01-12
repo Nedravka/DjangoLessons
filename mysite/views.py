2293941db16a876f3113bc4cfec813f9c3d993f1
SZakalyuzniy <zakalsergey@mail.ru>
2022-01-12 21:41:19 +0400

    create start page

---
+++ mysite/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from  django.template import loader
+
+def show_phones(request):
+    template = loader.get_template('product_page.html')
+    context = {}
+    rendered_page = template.render(context, request)
+    return HttpResponse(rendered_page)
